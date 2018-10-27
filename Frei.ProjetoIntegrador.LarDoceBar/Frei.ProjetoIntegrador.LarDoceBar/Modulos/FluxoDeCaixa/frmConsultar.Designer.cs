496ed8875bb61943ad8153c29da168d8190b27f0
daniel.anacleto2011 <daniel.anacleto2011@gmail.com>
2018-10-27 00:09:47 -0300

    design

--- Frei.ProjetoIntegrador.LarDoceBar/Frei.ProjetoIntegrador.LarDoceBar/Modulos/FluxoDeCaixa/frmConsultar.Designer.cs
+++ Frei.ProjetoIntegrador.LarDoceBar/Frei.ProjetoIntegrador.LarDoceBar/Modulos/FluxoDeCaixa/frmConsultar.Designer.cs
@@ -40,10 +40,10 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.dtpDia = new System.Windows.Forms.DateTimePicker();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label7 = new System.Windows.Forms.Label();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -68,9 +68,9 @@
             // lblSub
             // 
             this.lblSub.AutoSize = true;
-            this.lblSub.BackColor = System.Drawing.Color.Transparent;
+            this.lblSub.BackColor = System.Drawing.Color.Red;
             this.lblSub.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSub.ForeColor = System.Drawing.Color.Black;
+            this.lblSub.ForeColor = System.Drawing.Color.White;
             this.lblSub.Location = new System.Drawing.Point(270, 228);
             this.lblSub.Name = "lblSub";
             this.lblSub.Size = new System.Drawing.Size(60, 20);
@@ -80,9 +80,9 @@
             // lblGerais
             // 
             this.lblGerais.AutoSize = true;
-            this.lblGerais.BackColor = System.Drawing.Color.Transparent;
+            this.lblGerais.BackColor = System.Drawing.Color.Red;
             this.lblGerais.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblGerais.ForeColor = System.Drawing.Color.Black;
+            this.lblGerais.ForeColor = System.Drawing.Color.White;
             this.lblGerais.Location = new System.Drawing.Point(270, 127);
             this.lblGerais.Name = "lblGerais";
             this.lblGerais.Size = new System.Drawing.Size(60, 20);
@@ -92,9 +92,9 @@
             // lblFolha
             // 
             this.lblFolha.AutoSize = true;
-            this.lblFolha.BackColor = System.Drawing.Color.Transparent;
+            this.lblFolha.BackColor = System.Drawing.Color.Red;
             this.lblFolha.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFolha.ForeColor = System.Drawing.Color.Black;
+            this.lblFolha.ForeColor = System.Drawing.Color.White;
             this.lblFolha.Location = new System.Drawing.Point(270, 93);
             this.lblFolha.Name = "lblFolha";
             this.lblFolha.Size = new System.Drawing.Size(60, 20);
@@ -104,9 +104,9 @@
             // lblCompras
             // 
             this.lblCompras.AutoSize = true;
-            this.lblCompras.BackColor = System.Drawing.Color.Transparent;
+            this.lblCompras.BackColor = System.Drawing.Color.Red;
             this.lblCompras.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCompras.ForeColor = System.Drawing.Color.Black;
+            this.lblCompras.ForeColor = System.Drawing.Color.White;
             this.lblCompras.Location = new System.Drawing.Point(270, 58);
             this.lblCompras.Name = "lblCompras";
             this.lblCompras.Size = new System.Drawing.Size(60, 20);
@@ -116,9 +116,9 @@
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.BackColor = System.Drawing.Color.Red;
             this.label13.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.ForeColor = System.Drawing.Color.Black;
+            this.label13.ForeColor = System.Drawing.Color.White;
             this.label13.Location = new System.Drawing.Point(179, 228);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(68, 20);
@@ -128,9 +128,9 @@
             // lblVendas
             // 
             this.lblVendas.AutoSize = true;
-            this.lblVendas.BackColor = System.Drawing.Color.Transparent;
+            this.lblVendas.BackColor = System.Drawing.Color.Red;
             this.lblVendas.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblVendas.ForeColor = System.Drawing.Color.Black;
+            this.lblVendas.ForeColor = System.Drawing.Color.White;
             this.lblVendas.Location = new System.Drawing.Point(270, 26);
             this.lblVendas.Name = "lblVendas";
             this.lblVendas.Size = new System.Drawing.Size(60, 20);
@@ -140,9 +140,9 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.BackColor = System.Drawing.Color.Red;
             this.label9.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.ForeColor = System.Drawing.Color.White;
             this.label9.Location = new System.Drawing.Point(55, 127);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(104, 20);
@@ -152,9 +152,9 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.BackColor = System.Drawing.Color.Red;
             this.label6.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.ForeColor = System.Drawing.Color.White;
             this.label6.Location = new System.Drawing.Point(55, 93);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(205, 20);
@@ -164,9 +164,9 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.BackColor = System.Drawing.Color.Red;
             this.label4.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(55, 58);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(135, 20);
@@ -176,9 +176,9 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.BackColor = System.Drawing.Color.Red;
             this.label2.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(55, 26);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(142, 20);
@@ -192,21 +192,12 @@
             this.dtpDia.Size = new System.Drawing.Size(224, 20);
             this.dtpDia.TabIndex = 47;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Location = new System.Drawing.Point(339, 26);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(175, 104);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 46;
-            this.pictureBox1.TabStop = false;
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.BackColor = System.Drawing.Color.Red;
             this.label7.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
             this.label7.Location = new System.Drawing.Point(284, 133);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(285, 34);
@@ -215,8 +206,9 @@
             // 
             // btnBuscar
             // 
-            this.btnBuscar.BackColor = System.Drawing.Color.White;
+            this.btnBuscar.BackColor = System.Drawing.Color.Red;
             this.btnBuscar.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
             this.btnBuscar.Location = new System.Drawing.Point(552, 189);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(75, 34);
@@ -228,14 +220,26 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BackColor = System.Drawing.Color.Red;
             this.label1.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(203, 196);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(100, 20);
             this.label1.TabIndex = 43;
             this.label1.Text = "Consultar Dia:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Frei.ProjetoIntegrador.LarDoceBar.Properties.Resources.lar_doce_bar10;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(290, -7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(258, 137);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 77;
+            this.pictureBox1.TabStop = false;
             // 
             // frmConsultar
             // 
@@ -244,9 +248,9 @@
             this.BackgroundImage = global::Frei.ProjetoIntegrador.LarDoceBar.Properties.Resources.juniper_1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(831, 532);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.dtpDia);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.label1);
@@ -276,9 +280,9 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DateTimePicker dtpDia;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
