11980197498757c69c017f139c2dbfa3d6ed68a6
Pedro Henrique <PHM1080@outlook.com>
2018-11-09 00:24:01 -0200

    Connection Atualizada

--- Frei.ProjetoIntegrador.LarDoceBar/Frei.ProjetoIntegrador.LarDoceBar/DB/Base/Connection.cs
+++ Frei.ProjetoIntegrador.LarDoceBar/Frei.ProjetoIntegrador.LarDoceBar/DB/Base/Connection.cs
@@ -11,8 +11,14 @@ namespace Frei.ProjetoIntegrador.LarDoceBar.DB.Base
     {
         public MySqlConnection Create()
         {
-            //string connectionString = "server=104.214.59.125;database=lardocebarDB;uid=nsf;password=nsf@2018;sslmode=none";
+            //Base de dados Local
             string connectionString = "server=localhost;database=lardocebardb;uid=root;password=1234;sslmode=none";
+
+            //Base de dados FREI
+            //string connectionString = "server=192.168.0.100;database=lardocebardb;uid=nsf;password=nsf@2018;sslmode=none";
+
+            //Base de dados Azure
+            //string connectionString = "server=70.37.57.127;database=lardocebardb;uid=nsf;password=nsf@2018;sslmode=none";
 
             MySqlConnection connection = new MySqlConnection(connectionString);
             connection.Open();
